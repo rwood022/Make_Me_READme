a4ed78d179ffac74f5bf8b676984fce8b88f0dbd
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-01-19 22:11:48 -0500

    corrected alignment

--- index.js
+++ index.js
@@ -6,56 +6,58 @@ const { title } = require("process");
 // TODO: Create an array of questions for user input
 const questions = () => {
     return inqurer.prompt([
-    {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?",
-    },
-    {
-        type:"input",
-        name: "motivation",
-        message: "What motivated you to execute this project?",
-    },
-    {
-        type:"input",
-        name:"reason",
-        message: "What was the reason for buliding this assignment?",
-    },
-    {
-        type:"input",
-        name:"issue",
-        message: "What issue will your project fix?",
-    },
-    {
-        type:"input",
-        name:"learn",
-        message:"What did you did you learn?",
-    },
-    {
-        type:"input",
-        name:"installation",
-        message:"How can the user install your application?",
-    },
-    {
-        type:"input",
-        name:"usage",
-        message:"How can the user use your application?",
-    },
-    {
-        type:"input",
-        name:"credits",
-        message:"List any collaborators, third-party assets and/or tutorials that assisted with the making of your application:",
-    },
-    {
-        type: "selector",
-        name:"license",
-        message:"Pick a License",
-        options: ["MIT", "GNU GPLv3", "GNU AGPLv3", "GNU LGPv3", "Mozilla Public", "Apache", "Boost", "Unlicense"],
-    },
-]);
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?",
+        },
+        {
+            type:"input",
+            name: "motivation",
+            message: "What motivated you to execute this project?",
+        },
+        {
+            type:"input",
+            name:"reason",
+            message: "What was the reason for buliding this assignment?",
+        },
+        {
+            type:"input",
+            name:"issue",
+            message: "What issue will your project fix?",
+        },
+        {
+            type:"input",
+            name:"learn",
+            message:"What did you did you learn?",
+        },
+        {
+            type:"input",
+            name:"installation",
+            message:"How can the user install your application?",
+        },
+        {
+            type:"input",
+            name:"usage",
+            message:"How can the user use your application?",
+        },
+        {
+            type:"input",
+            name:"credits",
+            message:"List any collaborators, third-party assets and/or tutorials that assisted with the making of your application:",
+        },
+        {
+            type: "selector",
+            name:"license",
+            message:"Pick a License",
+            options: ["MIT", "GNU GPLv3", "GNU AGPLv3", "GNU LGPv3", "Mozilla Public", "Apache", "Boost", "Unlicense"],
+        },
+    ]);
+
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+const generateMd = ({ title, motivation, reason, issue, learn, installation, usage, credits, license }) =>
 `# ${title}
 
 
@@ -77,8 +79,7 @@ ${usage}
 ${credits}
 
 ## License
-${license}
-`}
+${license}`;
 
 // TODO: Create a function to initialize app
 function init() {
